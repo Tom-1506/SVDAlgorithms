93c5bfe53b5170e7959aa167c8805e584fee87ac
Tom <1599tm@gmail.com>
2019-02-09 16:13:07 +0000

    log linear solution implemented

--- src/SVDAlgorithms.java
+++ src/SVDAlgorithms.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SVDAlgorithms{
 
     /**
      * Takes int[] arrayIn and finds the SVD if it exists, using solution of worst
-     * runtime complexity O(n^2).
-     * returns the SVD if found or returns -1 if no SVD exists.
+     * runtime complexity O(n^2)
      * @param arrayIn
      * @return SVD of arrayIn or -1 if SVD doesn't exist
      */
@@ -30,14 +30,33 @@ public class SVDAlgorithms{
         return -1;
     }
 
-
+    /**
+     * Takes int[] arrayIn and finds the SVD if it exists, using solution of worst
+     * runtime complexity O(nlogn)
+     * @param arrayIn
+     * @return SVD of arrayIn or -1 if the SVD doesn't exist
+     */
     private static int logLinear(int[] arrayIn){
-        return 0;
+        Arrays.sort(arrayIn);
+        int count = 1;
+
+        for(int i = 0; i < arrayIn.length - 1; i++){
+            if(arrayIn[i] == arrayIn[i+1]){
+                count++;
+            } else{
+                count = 1;
+            }
+            if(count > arrayIn.length / 2){
+                return arrayIn[i];
+            }
+        }
+        System.out.println("No SVD exists");
+        return -1;
     }
 
     public static void main(String[] args){
-        int[] arr = {7, 7, 9, 3, 2, 7, 7};
+        int[] arr = {7, 7, 3, 6, 2, 7, 7};
 
-        System.out.println(quadratic(arr));
+        System.out.println(logLinear(arr));
     }
 }

360213aef3bc22116a5a27cc282f16fb006d969b
Tom <1599tm@gmail.com>
2019-02-28 20:45:50 +0000

    included functions for timing experiments

--- src/SVDAlgorithms.java
+++ src/SVDAlgorithms.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Random;
 
 public class SVDAlgorithms{
 
@@ -18,7 +15,7 @@ public class SVDAlgorithms{
         for(int i = 0; i < arrayIn.length; i++){
             count = 0;
             for (int j = 0; j < arrayIn.length; j++){
-                if (arrayIn[j] == arrayIn[i]){
+                if (arrayIn[j] == arrayIn[i]){ //fundamental op
                     count++;
                 }
             }
@@ -26,7 +23,7 @@ public class SVDAlgorithms{
                 return arrayIn[i];
             }
         }
-        System.out.println("No SVD exists");
+        //System.out.println("No SVD exists");
         return -1;
     }
 
@@ -41,7 +38,7 @@ public class SVDAlgorithms{
         int count = 1;
 
         for(int i = 0; i < arrayIn.length - 1; i++){
-            if(arrayIn[i] == arrayIn[i+1]){
+            if(arrayIn[i] == arrayIn[i+1]){ //fundamental op
                 count++;
             } else{
                 count = 1;
@@ -50,7 +47,7 @@ public class SVDAlgorithms{
                 return arrayIn[i];
             }
         }
-        System.out.println("No SVD exists");
+        //System.out.println("No SVD exists");
         return -1;
     }
 
@@ -67,7 +64,7 @@ public class SVDAlgorithms{
         int svd = -1;
 
         for(int i : arrayIn){
-            if(count == 0){
+            if(count == 0){ //fundamental op
                 choice = i;
                 count = 1;
                 continue;
@@ -80,30 +77,59 @@ public class SVDAlgorithms{
             }
         }
 
-        if(count == 0){
-            System.out.println("No SVD exists");
-        }
-        else{
-            count = 0;
-            for(int i : arrayIn){
-                if(choice == i){
-                    count++;
-                }
+        count = 0;
+        for(int i : arrayIn){
+            if(choice == i){ //fundamental op
+                count++;
             }
+        }
 
-            if(count > arrayIn.length / 2){
-                svd = choice;
-            }
-            else{
-                System.out.println("No SVD exists");
-            }
+        if(count > arrayIn.length / 2){
+            svd = choice;
         }
+        /*else{
+            System.out.println("No SVD exists");
+        }*/
         return svd;
     }
 
-    public static void main(String[] args){
-        int[] arr = {7, 7, 3, 6, 2, 7, 7};
+    public static int[] generateArray(int dimension){
+        int[] arrayOut = new int[dimension];
 
-        System.out.println(linear(arr));
+        Random randNum = new Random();
+
+        for(int i = 0; i < arrayOut.length; i++){
+            arrayOut[i] = randNum.nextInt(20) + 1;
+        }
+        return arrayOut;
+    }
+
+    public static void timeAlgo(int start, int iterSize, int numTests, int numIterations){
+        long startTime;
+        long endTime;
+        long average;
+        for(int i = 0; i < numTests; i++){
+            average = 0;
+
+            for(int j = 0; j < numIterations; j++){
+                int[] avgArray = generateArray(start + (iterSize * i));
+
+                startTime = System.nanoTime();
+                linear(avgArray);
+                endTime = System.nanoTime();
+
+                average += (endTime - startTime)/numIterations;
+            }
+            System.out.println(average);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {3,3,4,4,4};
+
+        timeAlgo(5000, 5000, 15, 10000);
+
+        System.out.println(logLinear(arr));
+
     }
 }
